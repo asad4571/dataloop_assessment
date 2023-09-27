05d77e008ff82cac4dc855954dd48ab9c10b2e2d
asad4571 <145261099+asad4571@users.noreply.github.com>
2023-09-27 22:00:54 +0500

    Add files via upload

---
+++ access_top_label_items_v2.py
@@ -0,0 +1,27 @@
+import dtlpy as dl
+dl.login()
+
+#Accessing the project and dataset
+project = dl.projects.get(project_id='ee06dd20-8c7e-4933-a67b-02b4de998a74')
+dataset = project.datasets.get(dataset_name='new_dataset')
+
+# Local path to the corresponding annotations - make sure the file names fit
+local_annotations_path = r'C:/home/project/annotations_folder'
+
+dataset.items.upload(local_path=r'C:/Users/asad.abbas/images_folder',
+                     local_annotations_path=r'C:/Users/asad.abbas/json_folder_1')
+
+#Creating a filter Query
+my_filter_top = dl.Filters()
+my_filter_top.add_join(field='label', values='top')
+
+
+pages = dataset.items.list(filters=my_filter_top)
+for item in pages.all():
+
+    print('item name')
+    print(item.name)
+
+    print("item id")
+    print(item.id)
+
